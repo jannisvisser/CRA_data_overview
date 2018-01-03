3a7dfe405a8b41ea75af1d2bc73ce86140f00092
Visser <jannisvisser@redcross.nl>
2018-01-03 16:03:39 +0100

    sunburst dynamic

--- js/site.js
+++ js/site.js
@@ -1,167 +1,28 @@
   
-	
-
-
-
-//Define charts & table items
-var country_chart = dc.rowChart("#countries");
-var admin_level_chart = dc.rowChart("#admin_levels");
-var dataTable = dc.dataTable("#dc-table-graph");
-var complete_chart = dc.rowChart('#complete');
-var recency_chart = dc.rowChart('#recency');
-var quality_chart = dc.rowChart('#quality');
-
-//Hide some charts initially
-$('#admin_levels').hide();
-$('#dc-table-graph').hide();
-$('.scores').hide();
-
-d3.dsv(';')("data/CRA_metadata.csv", function(dpi_data_full){
-	
-	console.log(dpi_data_full);
-	
-	var cf = crossfilter(dpi_data_full);
-	
-	cf.country = cf.dimension(function(d) {return d.country_code;});
-	cf.admin_level = cf.dimension(function(d) {return d.admin_level;});
-	cf.variable = cf.dimension(function(d) {return d.variable;});
-	cf.total = cf.dimension(function(d) {return 'Total';});
-	 
-	var country = cf.country.group(); 
-	var admin_level = cf.admin_level.group();
-	var completeness = cf.total.group().reduceSum(function(d) {return d.weight_corr;});
-	var recency = cf.total.group().reduceSum(function(d) {return d.recency_corr;});
-	var quality = cf.total.group().reduceSum(function(d) {return d.quality_corr;});
-	var all = cf.groupAll();
-	
-	country_chart.width(200).height(200)
-		.dimension(cf.country)
-		.group(country)
-		.elasticX(true)
-		.data(function(group) {
-			return group.top(6);
-		})
-		.colors(['#BF002D'])
-		.colorDomain([0,0])
-		.colorAccessor(function(d, i){return 1;})  
-		.on('filtered',function(chart,filters){
-			if (chart.filters().length == 1) { $('#admin_levels').show();}
-			else {
-				$('#admin_levels').hide();
-				$('#dc-table-graph').hide();
-				$('.scores').hide();
-				}
-		})
-		;
-
-	admin_level_chart.width(200).height(200)
-		.dimension(cf.admin_level)
-		.group(admin_level)
-		.elasticX(true)
-		.colors(['#BF002D'])
-		.colorDomain([0,0])
-		.colorAccessor(function(d, i){return 1;})  
-		.on('filtered',function(chart,filters){
-			if (chart.filters().length == 1) { 
-				$('#dc-table-graph').show();
-				$('.scores').show();
-				}
-			else {
-				$('#dc-table-graph').hide();
-				$('.scores').hide();
-				}
-		})
-		;
-	
-	complete_chart.width(200).height(50)
-		.margins({top: 0, left: 0, right: 0, bottom: 0})
-		.dimension(cf.total)
-		.group(completeness)
-		.colors(['#BF002D'])
-		.colorDomain([0,0])
-		.colorAccessor(function(d, i){return 1;}) 
-		.label(function(d){return 'Completeness: '.concat(Math.round(d.value*100)/100);})
-		.x(d3.scale.linear().range([0,(complete_chart.width()-50)]).domain([0,1]))
-		;
-	recency_chart.width(200).height(50)
-		.margins({top: 0, left: 0, right: 0, bottom: 0})
-		.dimension(cf.total)
-		.group(recency)
-		.colors(['#BF002D'])
-		.colorDomain([0,0])
-		.colorAccessor(function(d, i){return 1;}) 
-		.label(function(d){return 'Recency: '.concat(Math.round(d.value*100)/100);})
-		.x(d3.scale.linear().range([0,(recency_chart.width()-50)]).domain([0,1]))
-		;
-	quality_chart.width(200).height(50)
-		.margins({top: 0, left: 0, right: 0, bottom: 0})
-		.dimension(cf.total)
-		.group(quality)
-		.colors(['#BF002D'])
-		.colorDomain([0,0])
-		.colorAccessor(function(d, i){return 1;}) 
-		.label(function(d){return 'Quality: '.concat(Math.round(d.value*100)/100);})
-		.elasticX(false)
-		.x(d3.scale.linear().range([0,(quality_chart.width()-50)]).domain([0,1]))
-		//.xAxis().scale(quality_chart.x())
-		;	
-
-
-		
-	// Table of activities data
-	dataTable.width(960).height(800)
-		.dimension(cf.country)
-		.group(function(d) { return ""; })
-		.size(200)
-		.columns([
-		  //function(d) { return d.country_code; },
-		  //function(d) { return d.admin_level; },
-		  function(d) { return d.inform_level1; },
-		  function(d) { return d.inform_level2; },
-		  function(d) { return d.inform_level3; },
-		  function(d) { return d.label; },
-		  function(d) { return d.year; },
-		  function(d) { return "<a href='".concat(d.source_link).concat("' target='_blank'>Source link</a>"); }
-		])
-		.order(d3.ascending)
-		.sortBy(function (d) {
-			return d.id_overall; //[d.inform_level1,d.inform_level2,d.inform_level3,d.label].join();
-		})
-		;
-		
-	
-	dc.dataCount("#count-info")
-		.dimension(cf)
-		.group(all);
-	
-	
-    dc.renderAll();
-					       
-});
-
-
   ///////////////
  // SUNBURST ///
 ///////////////
 
 
 // Dimensions of sunburst.
-var width = 450;
+var width = 800;
 var height = 300;
 var radius = Math.min(width, height) / 2;
 
 // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
 var b = {
-  w: 75, h: 30, s: 3, t: 10
+  w: 175, h: 30, s: 3, t: 10
 };
 
 // Mapping of step names to colors.
 var colors = {
-  "Common Operational Datasets": "#5687d1",
-  "Administrative boundaries": "#5687d1",
+  //"Common Operational Datasets": "#5687d1",
+  "CODs": "#5687d1",
+  "Admin-boundaries": "#5687d1",
   "Population": "#5687d1",
  
-  "Hazard & Exposure": "#7b615c",
+  //"Hazard & Exposure": "#7b615c",
+  "Hazards": "#7b615c",
   "Natural": "#7b615c",
   "Human": "#7b615c",
   "Earthquake": "#7b615c",
@@ -173,7 +34,7 @@ var colors = {
   "Current Highly Violent Conflict Intensity": "#7b615c",
 
   "Vulnerability": "#de783b",
-  "SocioEconomic Vulnerability": "#de783b",
+  "Socio-Economic Vulnerability": "#de783b",
   "Vulnerable Groups": "#de783b",
   "Development & Deprivation": "#de783b",
   "Inequality": "#de783b",
@@ -184,7 +45,8 @@ var colors = {
   "Recent Shocks": "#de783b",
   "Food Security": "#de783b",
   
-  "Lack of Coping Capacity": "#6ab975",
+  //"Lack of Coping Capacity": "#6ab975",
+  "Coping Capacity": "#6ab975",
   "Institutional": "#6ab975",
   "Infrastructure": "#6ab975",
   "DRR": "#6ab975",
@@ -192,6 +54,8 @@ var colors = {
   "Communication": "#6ab975",
   "Physical infrastructure": "#6ab975",
   "Access to health care": "#6ab975",
+  
+  "yes": "#D3D3D3",
 };
 
 // Total size of all segments; we set this later, after loading the data.
@@ -212,21 +76,229 @@ var arc = d3.svg.arc()
     .startAngle(function(d) { return d.x; })
     .endAngle(function(d) { return d.x + d.dx; })
     .innerRadius(function(d) { return Math.sqrt(d.y); })
-    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });	
+
+
+//////////////////////
+// NORMAL VARIABLES //
+//////////////////////
+
+var country_code = '';
+
+//Define charts & table items
+var country_chart = dc.rowChart("#countries");
+var admin_level_chart = dc.rowChart("#admin_levels");
+var dataTable = dc.dataTable("#dc-table-graph");
+// var complete_chart = dc.rowChart('#complete');
+// var recency_chart = dc.rowChart('#recency');
+// var quality_chart = dc.rowChart('#quality');
+
+//Hide some charts initially
+$('#admin_levels').hide();
+$('#dc-table-graph').hide();
+$('#main').hide();
+$('.scores').hide();
+
+d3.dsv(';')("data/CRA_metadata.csv", function(dpi_data_full){
+	
+	d3.text("data/sunburst-input.csv", function(text) {
+		
+	    console.log(dpi_data_full);
+		
+		var cf = crossfilter(dpi_data_full);
+		
+		cf.country = cf.dimension(function(d) {return d.country_code;});
+		cf.admin_level = cf.dimension(function(d) {return d.admin_level;});
+		cf.variable = cf.dimension(function(d) {return d.variable;});
+		cf.total = cf.dimension(function(d) {return 'Total';});
+		
+		 
+		var country = cf.country.group();
+		var admin_level = cf.admin_level.group();
+		var completeness = cf.total.group().reduceSum(function(d) {return d.weight_corr;});
+		var recency = cf.total.group().reduceSum(function(d) {return d.recency_corr;});
+		var quality = cf.total.group().reduceSum(function(d) {return d.quality_corr;});
+		var all = cf.groupAll();
+		
+		
+		
+		initializeBreadcrumbTrail();
+		var test = function(country_code,admin_level = 2) {
+			
+			cf.test = cf.dimension(function(d) { if (d.country_code == country_code && d.admin_level >= admin_level) { return d.inform_level1 + '_' + d.inform_level2 + '_' + d.inform_level3;} } );
+			var test = cf.test.group().reduceSum(function(d) {return d.weight_overall;})
+			//console.log(test.top(Infinity));
+			
+			var csv_base = d3.csv.parseRows(text);
+			//var csv = [];
+			for (var j=0; j<csv_base.length;j++){
+				for (var i=0; i<test.top(Infinity).length;i++) {
+					if (csv_base[j][0] == test.top(Infinity)[i].key) {
+						csv_base[j][0] = csv_base[j][0] + '_yes';
+					}
+					// var array = [];
+					// array[0] = test.top(Infinity)[i].key;
+					// array[1] = test.top(Infinity)[i].value;
+					// csv[i] = array;
+				}
+			}
+			var json = buildHierarchy(csv_base);
+			createVisualization(json);
+			
+		}
+		//test();
+		
+		
+		country_chart.width(200).height(200)
+			.dimension(cf.country)
+			.group(country)
+			.elasticX(true)
+			.data(function(group) {
+				return group.top(6);
+			})
+			.colors(['#BF002D'])
+			.colorDomain([0,0])
+			.colorAccessor(function(d, i){return 1;})  
+			.label(function(d){return d.key || ': ' || d.value;})
+			.on('filtered',function(chart,filters){
+				if (chart.filters().length > 1) {chart.filters().shift();} 
+				if (chart.filters().length == 1) { 
+					country_code = chart.filters()[0];
+					test(country_code);
+					$('#admin_levels').show();
+					$('#dc-table-graph').show();
+					$('#main').show();
+				} else {
+					$('#admin_levels').hide();
+					$('#dc-table-graph').hide();
+					$('#main').hide();
+					$('.scores').hide();
+				};
+			})
+			;
+
+		admin_level_chart.width(200).height(200)
+			.dimension(cf.admin_level)
+			.group(admin_level)
+			.elasticX(true)
+			.colors(['#BF002D'])
+			.colorDomain([0,0])
+			.colorAccessor(function(d, i){return 1;})  
+			.on('filtered',function(chart,filters){
+				if (chart.filters().length > 1) {chart.filters().shift();} 
+				if (chart.filters().length == 1) { 
+					var admin_level = chart.filters()[0];
+					test(country_code,admin_level);
+				}
+			})
+			;
+		
+		// complete_chart.width(200).height(50)
+			// .margins({top: 0, left: 0, right: 0, bottom: 0})
+			// .dimension(cf.total)
+			// .group(completeness)
+			// .colors(['#BF002D'])
+			// .colorDomain([0,0])
+			// .colorAccessor(function(d, i){return 1;}) 
+			// .label(function(d){return 'Completeness: '.concat(Math.round(d.value*100)/100);})
+			// .x(d3.scale.linear().range([0,(complete_chart.width()-50)]).domain([0,1]))
+			// ;
+		// recency_chart.width(200).height(50)
+			// .margins({top: 0, left: 0, right: 0, bottom: 0})
+			// .dimension(cf.total)
+			// .group(recency)
+			// .colors(['#BF002D'])
+			// .colorDomain([0,0])
+			// .colorAccessor(function(d, i){return 1;}) 
+			// .label(function(d){return 'Recency: '.concat(Math.round(d.value*100)/100);})
+			// .x(d3.scale.linear().range([0,(recency_chart.width()-50)]).domain([0,1]))
+			// ;
+		// quality_chart.width(200).height(50)
+			// .margins({top: 0, left: 0, right: 0, bottom: 0})
+			// .dimension(cf.total)
+			// .group(quality)
+			// .colors(['#BF002D'])
+			// .colorDomain([0,0])
+			// .colorAccessor(function(d, i){return 1;}) 
+			// .label(function(d){return 'Quality: '.concat(Math.round(d.value*100)/100);})
+			// .elasticX(false)
+			// .x(d3.scale.linear().range([0,(quality_chart.width()-50)]).domain([0,1]))
+			//.xAxis().scale(quality_chart.x())
+			// ;	
+
+
+			
+		// Table of activities data
+		dataTable.width(960).height(800)
+			.dimension(cf.country)
+			.group(function(d) { return ""; })
+			.size(200)
+			.columns([
+			  //function(d) { return d.country_code; },
+			  //function(d) { return d.admin_level; },
+			  function(d) { return d.inform_level1; },
+			  function(d) { return d.inform_level2; },
+			  function(d) { return d.inform_level3; },
+			  function(d) { return d.label; },
+			  function(d) { return d.year; },
+			  function(d) { return "<a href='".concat(d.source_link).concat("' target='_blank'>Source link</a>"); }
+			])
+			.order(d3.ascending)
+			.sortBy(function (d) {
+				return d.id_overall; //[d.inform_level1,d.inform_level2,d.inform_level3,d.label].join();
+			})
+			;
+			
+		
+		dc.dataCount("#count-info")
+			.dimension(cf)
+			.group(all);
+		
+		
+		dc.renderAll();
+							   
+	})
+});
+
+
+ 
 
 // Use d3.text and d3.csv.parseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("data/sunburst-input.csv", function(text) {
-  var csv = d3.csv.parseRows(text);
-  var json = buildHierarchy(csv);
-  createVisualization(json);
-});
+// d3.text("data/sunburst-input.csv", function(text) {
+  // var csv = d3.csv.parseRows(text);
+  // console.log(csv);
+  // var json = buildHierarchy(csv);
+  // createVisualization(json);
+// });
+
 
 // Main function to draw and set up the visualization, once we have the data.
 function createVisualization(json) {
 
+  // Total size of all segments; we set this later, after loading the data.
+	totalSize = 0; 
+
+	d3.select("#chart").select("svg").remove();
+	var vis = d3.select("#chart").append("svg:svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("svg:g")
+		.attr("id", "container")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	partition = d3.layout.partition()
+		.size([2 * Math.PI, radius * radius])
+		.value(function(d) { return d.size; });
+
+	arc = d3.svg.arc()
+		.startAngle(function(d) { return d.x; })
+		.endAngle(function(d) { return d.x + d.dx; })
+		.innerRadius(function(d) { return Math.sqrt(d.y); })
+		.outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+				
   // Basic setup of page elements.
-  initializeBreadcrumbTrail();
+  //initializeBreadcrumbTrail();
   drawLegend();
   d3.select("#togglelegend").on("click", toggleLegend);
 
@@ -448,7 +520,7 @@ function buildHierarchy(csv) {
     if (isNaN(size)) { // e.g. if this is a header row
       continue;
     }
-    var parts = sequence.split("-");
+    var parts = sequence.split("_");
     var currentNode = root;
     for (var j = 0; j < parts.length; j++) {
       var children = currentNode["children"];
